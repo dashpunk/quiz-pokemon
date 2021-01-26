1fe693eb42bc88e1e99f9cb8e300b790246ed3ec
Willians Andrade <willians.lemos87@gmail.com>
2021-01-26 12:29:51 -0300

    Commit para Aula 02 da Imersão React

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import styled from 'styled-components'
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import db from '../db.json';
 import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo'
@@ -18,8 +21,14 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Pokémon Quiz - Alura</title>
+      </Head>
       <QuizContainer>
         <QuizLogo srcImage={db.logo}></QuizLogo>
         <Widget>
@@ -28,6 +37,25 @@ export default function Home() {
           </Widget.Header>
           <Widget.Content>
             <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+                  // State
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
